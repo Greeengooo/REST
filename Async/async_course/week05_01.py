f2d184a3d558452767ba3b7eaba257c7f36b8690
Greeengooo <stetsyk>
2022-01-07 16:45:07 +0200

    refacor1

--- Async/async_course/week05_01.py
+++ Async/async_course/week05_01.py
@@ -10,28 +10,29 @@ class Client:
         self._port = port
         self._timeout = timeout
 
-    def send_to_server(self, message: str) -> str:
+    def send_and_fetch(self, message: str) -> str:
         with socket.create_connection((self._host, self._port), self._timeout) as sock:
             sock.sendall(message.encode("utf8"))
-            server = sock.recv(1024)
-            return server.decode("utf8")
+            server_response = sock.recv(1024)
+            return server_response.decode("utf8")
 
     def get(self, metric_name: str) -> dict:
-        server_response = self.send_to_server(f"get {metric_name}\n")
-        if server_response == 'ok\n\n':
+        server_response = self.send_and_fetch(f"get {metric_name}\n")
+        if server_response == "ok\n\n":
             return {}
-        valid_response = self.is_valid_response(server_response)
-        if not valid_response:
+        if not self.is_valid_response_data(server_response):
             raise ClientError
         return self.create_dict(server_response)
 
-    def is_valid_response(self, response: str) -> bool:
+
+    def is_valid_response_data(self, response: str) -> bool:
         splitted_response = response.strip().split('\n')
         if splitted_response[0] == "ok":
-            for i in splitted_response[1:]:
-                if not re.match(r"(.+)\s(\d+\.*\d*)\s(\d+)", i):
-                    return False
-            return True
+            return all(
+                re.match(r"(.+)\s(\d+\.*\d*)\s(\d+)", i)
+                for i in splitted_response[1:]
+            )
+
         return False
 
     def create_dict(self, response: str) -> dict:
@@ -40,17 +41,17 @@ class Client:
         for elem in splitted_response_no_status:
             metric_name, metric_value, timestamp = elem.split()
             temp[metric_name].append((int(timestamp), float(metric_value)))
-        return dict((key, sorted(val, key=lambda x: x[0])) for key, val in temp.items())
+        return {key: sorted(val, key=lambda x: x[0]) for key, val in temp.items()}
 
     def put(self, metric_name: str, metric_value: int, timestamp: int = None) -> None:
         if timestamp is None:
             timestamp = int(time.time())
-        server_response = self.send_to_server(f"put {metric_name} {metric_value} {timestamp}\n")
+        server_response = self.send_and_fetch(f"put {metric_name} {metric_value} {timestamp}\n")
         if server_response == 'ok\n\n':
             return
-        valid_response = self.is_valid_response(server_response)
-        if not valid_response:
+        if not self.is_valid_response_data(server_response):
             raise ClientError
+        return
 
 
 class ClientError(Exception):
